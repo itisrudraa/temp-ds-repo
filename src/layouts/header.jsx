3b38c8a7641877ef861df7fe19c8687485bfa610
itisrudraa <merudra01@gmail.com>
2026-03-31 22:55:31 +0530

    updated header structure

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -1,36 +1,32 @@
 import { useTheme } from "@/hooks/use-theme";
-
-import {Moon, Sun, Bell } from "lucide-react";
-
+import { Moon, Sun, Bell } from "lucide-react";
 import profileImg from "@/assets/profile-image.jpg";
 
-import PropTypes from "prop-types";
-
-export const Header = ({ collapsed, setCollapsed }) => {
+export const Header = () => {
     const { theme, setTheme } = useTheme();
 
     return (
         <header className="relative z-10 flex h-[60px] items-center justify-between bg-white px-4 shadow-md transition-colors dark:bg-slate-900">
+            
             <h1 className="text-2xl font-bold text-slate-900 dark:text-white">
                 OurVault
             </h1>
+
             <div className="flex items-center gap-x-3">
+                
                 <button
                     className="btn-ghost size-10"
                     onClick={() => setTheme(theme === "light" ? "dark" : "light")}
                 >
-                    <Sun
-                        size={20}
-                        className="dark:hidden"
-                    />
-                    <Moon
-                        size={20}
-                        className="hidden dark:block"
-                    />
+                    <Sun size={20} className="dark:hidden" />
+                    <Moon size={20} className="hidden dark:block" />
                 </button>
+
                 <button className="btn-ghost size-10">
                     <Bell size={20} />
                 </button>
+
+
                 <button className="size-10 overflow-hidden rounded-full">
                     <img
                         src={profileImg}
@@ -41,9 +37,4 @@ export const Header = ({ collapsed, setCollapsed }) => {
             </div>
         </header>
     );
-};
-
-Header.propTypes = {
-    collapsed: PropTypes.bool,
-    setCollapsed: PropTypes.func,
 };
