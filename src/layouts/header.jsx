cc07a601ff46391039328bc110e0e248fba2ba29
itisrudraa <merudra01@gmail.com>
2026-03-31 22:00:29 +0530

    Updated header

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -1,6 +1,6 @@
 import { useTheme } from "@/hooks/use-theme";
 
-import { Bell, ChevronsLeft, Moon, Search, Sun } from "lucide-react";
+import {Moon, Sun, Bell } from "lucide-react";
 
 import profileImg from "@/assets/profile-image.jpg";
 
@@ -11,27 +11,9 @@ export const Header = ({ collapsed, setCollapsed }) => {
 
     return (
         <header className="relative z-10 flex h-[60px] items-center justify-between bg-white px-4 shadow-md transition-colors dark:bg-slate-900">
-            <div className="flex items-center gap-x-3">
-                <button
-                    className="btn-ghost size-10"
-                    onClick={() => setCollapsed(!collapsed)}
-                >
-                    <ChevronsLeft className={collapsed && "rotate-180"} />
-                </button>
-                <div className="input">
-                    <Search
-                        size={20}
-                        className="text-slate-300"
-                    />
-                    <input
-                        type="text"
-                        name="search"
-                        id="search"
-                        placeholder="Search..."
-                        className="w-full bg-transparent text-slate-900 outline-0 placeholder:text-slate-300 dark:text-slate-50"
-                    />
-                </div>
-            </div>
+            <h1 className="text-2xl font-bold text-slate-900 dark:text-white">
+                OurVault
+            </h1>
             <div className="flex items-center gap-x-3">
                 <button
                     className="btn-ghost size-10"
